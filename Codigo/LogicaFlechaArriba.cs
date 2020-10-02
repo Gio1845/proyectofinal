5304af10ed72eadd427153607d97a6d126d78fd0
Gio1845 <victor-reyesr@hotmail.com>
2020-10-01 19:55:29 -0600

    assestscommit

---
+++ Codigo/LogicaFlechaArriba.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicaFlechaArriba : MonoBehaviour
+{
+   public float velocidad;
+    public int contador = 0;
+    public bool adentro = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      transform.position+=transform.right*-velocidad*Time.deltaTime;
+      if(contador == 2)
+      {
+          adentro = true;
+      }
+      else
+      {
+          adentro=false;
+      }
+      if(Input.GetKeyDown(KeyCode.UpArrow))
+      {
+          if(adentro)
+          {
+              GameObject.Find("CasillaJugador").GetComponent<LogicaJugador>().puntaje++;
+               GameObject.Find("CasillaJugador").GetComponent<LogicaJugador>().texto.text="Score: "+
+                GameObject.Find("CasillaJugador").GetComponent<LogicaJugador>().puntaje.ToString();
+            Destroy(gameObject);
+                
+
+          }
+      }
+        
+    }
+    public void OnTriggerEnter2D(Collider2D collision) 
+    {
+      if(collision.gameObject.tag=="Player")
+      {
+          contador++;
+      }  
+    }
+    public void OnTriggerExit2D(Collider2D collision) 
+    {
+      if(collision.gameObject.tag=="Player")
+      {
+          contador--;
+      }  
+    }
+}
